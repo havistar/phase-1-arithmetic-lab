3f3dcf0385df83a76614135c554d48278a675589
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-11 16:07:27 -0500

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2 
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 28
+const num4 = 6
+const mod = num3 % num4 
+
+let max = Math.max(20);
